0416b109317c720c054b5e88ede06f55e1967ed7
Sabih Ahmed <sahmed.dic@emaar.ae>
2020-04-06 02:18:24 +0400

    Screen Weather List Test

--- app/src/androidTest/java/com/sahmed/forecaster/framework/presentation/cities_weather/ScreenCityWeathersListTest.kt
+++ app/src/androidTest/java/com/sahmed/forecaster/framework/presentation/cities_weather/ScreenCityWeathersListTest.kt
@@ -1,15 +1,20 @@
 package com.sahmed.forecaster.framework.presentation.cities_weather
 
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.sahmed.forecaster.framework.presentation.landing.MainFragment
 import com.sahmed.forecaster.R
 import com.sahmed.forecaster.framework.presentation.landing.MainActivity
+import org.hamcrest.core.IsNot.not
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
@@ -18,22 +23,25 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ScreenCityWeathersListTest{
 
+    @get:Rule
+    val activityRule =  ActivityTestRule(MainActivity::class.java,false,false)
+
 
     @Test
     fun checkIfSearchFieldIsInputReady(){
-        launchFragmentInContainer<MainFragment>()
+        activityRule.launchActivity(null)
+        onView(ViewMatchers.withId(R.id.btn_cities_weather)).perform(ViewActions.click())
+        onView(withId(R.id.search_field)).check(matches(isDisplayed()))
+        onView(withId(R.id.search_field)).check(matches(isEnabled()))
 
-        onView(withId(R.id.rv_city_weathers)).check(matches(isDisplayed()))
     }
 
     @Test
     fun checkIfProgressIsNotVisibleInitially(){
-
+        activityRule.launchActivity(null)
+        onView(ViewMatchers.withId(R.id.btn_cities_weather)).perform(ViewActions.click())
+        onView(withId(R.id.progress)).check(matches(not(isDisplayed())))
     }
 
-    @Test
-    fun checkCityNamesShowValidationError(){
-
-    }
 
 }
